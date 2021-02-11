c53215e958a965a1fb95744d90164e982e85c7eb
Andrew Cooper <andrew.cooper3@citrix.com>
2021-02-11 19:55:06 +0000

    efi (arm?)
      boot.c: In function 'efi_start':
      boot.c:1339:9: error: 'argc' may be used uninitialized in this function [-Werror=maybe-uninitialized]
       1339 |         efi_arch_handle_cmdline(argc ? *argv : NULL, options, name.s);
            |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- xen/common/efi/boot.c
+++ xen/common/efi/boot.c
@@ -1126,7 +1126,7 @@ efi_start(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
     static EFI_GUID __initdata shim_lock_guid = SHIM_LOCK_PROTOCOL_GUID;
     EFI_LOADED_IMAGE *loaded_image;
     EFI_STATUS status;
-    unsigned int i, argc;
+    unsigned int i, argc = 0;
     CHAR16 **argv, *file_name, *cfg_file_name = NULL, *options = NULL;
     UINTN gop_mode = ~0;
     EFI_SHIM_LOCK_PROTOCOL *shim_lock;
