cdbb2c873b882610d239546d5ad6a120beccb5cc
Frediano Ziglio <frediano.ziglio@cloud.com>
2024-10-07 10:32:36 +0100

    Dump entire MBI buffer
    Try to understand what went wrong with the patch to check better
    this structure.

--- xen/arch/x86/efi/mbi2.c
+++ xen/arch/x86/efi/mbi2.c
@@ -5,6 +5,49 @@
 #include <xen/multiboot2.h>
 #include <asm/asm_defns.h>
 #include <asm/efi.h>
+#include <asm/io.h>
+
+static void __init
+out_char(char c)
+{
+    enum {
+        io_base = 0x3f8,
+        data_port = io_base,
+        status_port = io_base + 5,
+
+        thr_empty = 0x20,
+    };
+
+    /* wait space available */
+    while ((inb(status_port) & thr_empty) == 0)
+        continue;
+    outb(c, data_port);
+}
+
+static void __init
+out_string(const char *s)
+{
+    for (; *s; ++s)
+        out_char(*s);
+}
+
+static void __init
+out_byte_hex(uint8_t byte)
+{
+    static const char hex[] = "0123456789abcdef";
+    out_char(hex[(byte >> 4) & 0xf]);
+    out_char(hex[(byte >> 0) & 0xf]);
+    out_char(',');
+}
+
+static void __init
+out_buf(const void *start, const void *end)
+{
+    out_string("Buffer: ");
+    for (const uint8_t *p = start; (const void *)p < end; ++p)
+        out_byte_hex(*p);
+    out_string("\r\n");
+}
 
 const char * asmlinkage __init
 efi_multiboot2_prelude(uint32_t magic, const multiboot2_fixed_t *mbi)
@@ -14,9 +57,19 @@ efi_multiboot2_prelude(uint32_t magic, const multiboot2_fixed_t *mbi)
     EFI_SYSTEM_TABLE *SystemTable = NULL;
     const char *cmdline = NULL;
     bool have_bs = false;
+    const void *mbi_copy = mbi;
 
     if ( magic != MULTIBOOT2_BOOTLOADER_MAGIC )
         return "ERR: Not a Multiboot2 bootloader!";
+
+    /* dump header pointer */
+    out_buf(&mbi_copy, (&mbi_copy) + 1);
+
+    /* dump just header */
+    out_buf(mbi, mbi + 1);
+
+    /* dump all buffer */
+    out_buf(mbi, (const void *)mbi + mbi->total_size);
 
     /* Skip Multiboot2 information fixed part. */
     tag = _p(ROUNDUP((unsigned long)(mbi + 1), MULTIBOOT2_TAG_ALIGN));
