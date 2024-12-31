21ac6beaa36df56c68591c05554ee613f735f2a0
Andrew Cooper <andrew.cooper3@citrix.com>
2024-12-31 15:56:34 +0000

    x86/traps: Move percpu_traps_init() into traps-init.c

--- xen/arch/x86/traps-init.c
+++ xen/arch/x86/traps-init.c
@@ -3,6 +3,17 @@
  * Configuration of event handling for all CPUs.
  */
 #include <asm/idt.h>
+#include <asm/msr.h>
 #include <asm/page.h>
+#include <asm/system.h>
+#include <asm/traps.h>
 
 DEFINE_PER_CPU_READ_MOSTLY(idt_entry_t *, idt);
+
+void percpu_traps_init(void)
+{
+    subarch_percpu_traps_init();
+
+    if ( cpu_has_xen_lbr )
+        wrmsrl(MSR_IA32_DEBUGCTLMSR, IA32_DEBUGCTLMSR_LBR);
+}
